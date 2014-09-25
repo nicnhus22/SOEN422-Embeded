c6d122d2f1d4c1e5da13784ed966239420007d81
nicnhus22 <nicolas.husser@gmail.com>
2014-09-24 20:59:21 -0400

    Create Lab2_ex6.c

---
+++ Lab2_ex6.c
@@ -0,0 +1,38 @@
+   
+   
+   #include <avr/io.h>
+   #include <util/delay.h>
+   #include <stdio.h>
+   
+   #include "usb_serial.h"
+   
+   #define CPU_PRESCALE(n) (CLKPR = 0x80, CLKPR = (n))
+   #define F_CPU 16000000L
+   #define CPU_16MHz 0x00
+      
+   
+   int main(){
+   	usb_init();
+   	
+        CPU_PRESCALE(CPU_16MHz);
+  
+  	while(1){
+  		usb_serial_putchar('a');
+		
+		//Select ADC Channel ch must be 0-7
+		ADMUX |= (1 << REFS0) | 0x2;
+		
+		//Start Single conversion
+		ADCSRA|=(1<<ADSC);
+		
+		//Wait for conversion to complete
+		while(!(ADCSRA & (1<<ADIF)));
+		
+		//Clear ADIF by writing one to it
+		ADCSRA|=(1<<ADIF);
+		
+		_delay_ms(1000);
+	
+  	}
+   }
+                               
