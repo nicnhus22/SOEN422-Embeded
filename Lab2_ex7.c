bc6430202c6434962549c70984194c0afe66940d
nicnhus22 <nicolas.husser@gmail.com>
2014-09-30 16:21:39 -0400

    Update Lab2_ex7.c

--- Lab2_ex7.c
+++ Lab2_ex7.c
@@ -11,46 +11,46 @@
    int count; 
    ISR(TIMER0_OVF_vect)
    {
+   	usb_serial_putchar('a');
    	count++;
   	if(count==1000)
   	{
   		//Select ADC Channel ch must be 0-7
-		//ADMUX |= (1 << REFS0) | 0x2;
+		ADMUX |= (1 << REFS0) | 0x2;
 	
 		//Start Single conversion
-		//ADCSRA|=(1<<ADSC);
+		ADCSRA|=(1<<ADSC);
 	
 		//Wait for conversion to complete
-		//while(!(ADCSRA & (1<<ADIF)));
+		while(!(ADCSRA & (1<<ADIF)));
 	
 		//Clear ADIF by writing one to it
-		//ADCSRA|=(1<<ADIF);
+		ADCSRA|=(1<<ADIF);
   		
-	  	//count = 0;
-	  	 }	
-	}
+	  	count = 0;
+	}	
+   }
 	 
 	
-        int main(){ 
+int main(){ 
 
-
+	CPU_PRESCALE(CPU_16MHz);
+	
 	usb_init();
-	 usb_serial_putchar('a');	
+		
 	//fast PWM and toggle OC0A on compare match 
 	TCCR0A |= (1<<COM0A0) | (1<<WGM01);
 
 	//prescaler of clk/1024
 	TCCR0B |= (1<<CS00) | (1<<CS02); 
 
-	// Period is roughly 1.2 seconds.
-	//OCR1A = 20000;
-
 	TIMSK0 |= (1 << TOIE0);
-	  TCNT0=0;
-          CPU_PRESCALE(CPU_16MHz);
-	  sei();
+	
+	TCNT0=0;
+          
+	sei();
 	  
-	  while(1){
+	while(1){
           int i=1;
           while(i<=255){
           	 PORTD |= (1<<6);
