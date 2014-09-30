7da70d5ed7d264faaf191bd675b8c6f2df6577a5
Callisto1 <jeansimon55@gmail.com>
2014-09-30 15:53:29 -0400

    Update Lab2_ex6.c

--- Lab2_ex6.c
+++ Lab2_ex6.c
@@ -1,38 +1,43 @@
-   
-   
-   #include <avr/io.h>
-   #include <util/delay.h>
-   #include <stdio.h>
-   
-   #include "usb_serial.h"
-   
-   #define CPU_PRESCALE(n) (CLKPR = 0x80, CLKPR = (n))
-   #define F_CPU 16000000L
-   #define CPU_16MHz 0x00
-      
-   
-   int main(){
-   	CPU_PRESCALE(CPU_16MHz);
-   	
-   	usb_init();
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdio.h>
+#include "usb_serial.h"
+#define CPU_PRESCALE(n) (CLKPR = 0x80, CLKPR = (n))
+#define F_CPU 16000000L
+#define CPU_16MHz 0x00
+
+
+
+int main(){
+	CPU_PRESCALE(CPU_16MHz);
+	usb_init();
+	int value = 0;
   
+  	char buf[32];
   	while(1){
-  		usb_serial_putchar('a');
+  		
 		
 		//Select ADC Channel ch must be 0-7
-		ADMUX |= (1 << REFS0) | 0x2;
+		ADMUX |= (1 << REFS0) | 0x1;
 		
-		//Start Single conversion
-		ADCSRA|=(1<<ADSC);
+		//Start Single conversion		
+		
+		ADCSRA=(1<<ADEN)|(1<<ADPS2)|(ADPS1)|(ADPS0);
+		
+		ADCSRA=(1<<ADSC);
+
 		
 		//Wait for conversion to complete
-		while(!(ADCSRA & (1<<ADIF)));
+		if(ADCSRA & (1<<ADIF)){
+			value = ADC;
+			usb_serial_write(ADC,10);
+			
+		}
 		
+			
 		//Clear ADIF by writing one to it
-		ADCSRA|=(1<<ADIF);
-		
-		_delay_ms(1000);
+		ADCSRA|=(1<<ADIF);	
 	
   	}
-   }
-                               
+	
+}
